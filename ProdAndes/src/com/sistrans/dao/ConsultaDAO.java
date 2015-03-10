8315c37998512ef4c4c747466f8a3c4df2574313
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2015-03-10 14:52:38 -0500

    Upload

---
+++ ProdAndes/src/com/sistrans/dao/ConsultaDAO.java
@@ -0,0 +1,74 @@
+package com.sistrans.dao;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class ConsultaDAO {
+	
+	//Constantes
+	
+	private static final String ARCHIVO_CONEXION = "/../conexion.properties";
+	
+	//Consultas
+	//Atributos
+	
+	public Connection conexion;
+	
+	private String usuario;
+	
+	private String clave;
+	
+	private String cadenaConexion;
+	
+	public ConsultaDAO()
+	{
+		
+	}
+	
+	//Metodos
+	
+	public void inicializar(String path)
+	{
+		try {
+			File arch = new File(path + ARCHIVO_CONEXION);
+			Properties prop = new Properties();
+			FileInputStream in = new FileInputStream(arch);
+			
+			prop.load(in);
+			in.close();
+			
+			usuario = prop.getProperty("usuario");
+			clave = prop.getProperty("clave");
+			final String driver = prop.getProperty("driver");
+			Class.forName(driver);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void establecerConexion(String url, String usuario, String clave) throws SQLException
+	{
+		try
+		{
+			conexion = DriverManager.getConnection(url, usuario, clave);
+		} catch(Exception e)
+		{
+			throw new SQLException("ERROR: ConsultaDAO obteniendo una conexin.");
+		}
+	}
+	
+	public void closeConnection(Connection connection) throws Exception
+	{
+		try
+		{
+			connection.close();
+			connection = null;
+		} catch(Exception e)
+		{
+			throw new Exception("ERROR: ConsultaDAO: closeConnection() = cerrando una conexión.");
+		}
+	}
+}
