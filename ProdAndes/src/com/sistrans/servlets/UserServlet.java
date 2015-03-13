8a62cb91d768923a1263360f9d7648dca197af3b
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2015-03-13 09:44:25 -0500

    Servlets de roles funcionando

---
+++ ProdAndes/src/com/sistrans/servlets/UserServlet.java
@@ -0,0 +1,55 @@
+package com.sistrans.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/user.html")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		procesarSolicitud(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		procesarSolicitud(request, response);
+	}
+	public void procesarSolicitud(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		PrintWriter out = response.getWriter();
+		printHeader(response, out);
+		
+		String action = request.getParameter("submit");
+		
+		out.println(action);
+	}
+
+	private void printHeader(HttpServletResponse response, PrintWriter out){
+		out.println("<!DOCTYPE html><html><head></title></head>");
+	}
+}
