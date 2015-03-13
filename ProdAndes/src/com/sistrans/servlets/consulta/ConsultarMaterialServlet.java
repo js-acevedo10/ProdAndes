1400d173a14888574887631c770240010b53db40
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2015-03-13 16:43:29 -0500

    Added Consulta de material y existencias

--- ProdAndes/src/com/sistrans/servlets/consulta/ConsultarMaterialServlet.java
+++ ProdAndes/src/com/sistrans/servlets/consulta/ConsultarMaterialServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MaterialServlet
  */
-@WebServlet("/ConsultarMaterialServlet")
+@WebServlet("/consultar/material.html")
 public class ConsultarMaterialServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
