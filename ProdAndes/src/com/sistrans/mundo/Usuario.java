e4522069e530bfd195b127b5c403baa682ae93f9
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2015-03-15 14:05:49 -0500

    usuario

---
+++ ProdAndes/src/com/sistrans/mundo/Usuario.java
@@ -0,0 +1,316 @@
+package com.sistrans.mundo;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Usuario
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String login;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String clave;
+	
+	/**
+	 * @return the login
+	 */
+	public String getLogin() {
+		return login;
+	}
+
+	/**
+	 * @param login the login to set
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	/**
+	 * @return the clave
+	 */
+	public String getClave() {
+		return clave;
+	}
+
+	/**
+	 * @param clave the clave to set
+	 */
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	/**
+	 * @return the tipoDoc
+	 */
+	public String getTipoDoc() {
+		return tipoDoc;
+	}
+
+	/**
+	 * @param tipoDoc the tipoDoc to set
+	 */
+	public void setTipoDoc(String tipoDoc) {
+		this.tipoDoc = tipoDoc;
+	}
+
+	/**
+	 * @return the numDoc
+	 */
+	public String getNumDoc() {
+		return numDoc;
+	}
+
+	/**
+	 * @param numDoc the numDoc to set
+	 */
+	public void setNumDoc(String numDoc) {
+		this.numDoc = numDoc;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the direccion
+	 */
+	public String getDireccion() {
+		return direccion;
+	}
+
+	/**
+	 * @param direccion the direccion to set
+	 */
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	/**
+	 * @return the nacionalidad
+	 */
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	/**
+	 * @param nacionalidad the nacionalidad to set
+	 */
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the telefono
+	 */
+	public int getTelefono() {
+		return telefono;
+	}
+
+	/**
+	 * @param telefono the telefono to set
+	 */
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	/**
+	 * @return the ciudad
+	 */
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	/**
+	 * @param ciudad the ciudad to set
+	 */
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	/**
+	 * @return the departamento
+	 */
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	/**
+	 * @param departamento the departamento to set
+	 */
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	/**
+	 * @return the codPostal
+	 */
+	public String getCodPostal() {
+		return codPostal;
+	}
+
+	/**
+	 * @param codPostal the codPostal to set
+	 */
+	public void setCodPostal(String codPostal) {
+		this.codPostal = codPostal;
+	}
+
+	public Usuario(String login, String clave, String tipoDoc, String numDoc,
+			String nombre, String direccion, String nacionalidad, String email,
+			int telefono, String ciudad, String departamento, String codPostal) {
+		super();
+		this.login = login;
+		this.clave = clave;
+		this.tipoDoc = tipoDoc;
+		this.numDoc = numDoc;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.nacionalidad = nacionalidad;
+		this.email = email;
+		this.telefono = telefono;
+		this.ciudad = ciudad;
+		this.departamento = departamento;
+		this.codPostal = codPostal;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String tipoDoc;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String numDoc;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String nombre;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String direccion;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String nacionalidad;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String email;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private int telefono;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String ciudad;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String departamento;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String codPostal;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Usuario(){
+		super();
+	}
+
+}
+
