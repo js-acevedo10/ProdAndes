24b9a66181a1b38eb7fa41567b37486709ce2dfa
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2015-05-20 21:23:34 -0500

    fx

--- ProdAndes/src/com/sistrans/dao/Zend.java
+++ ProdAndes/src/com/sistrans/dao/Zend.java
@@ -1,16 +1,15 @@
 package com.sistrans.dao;
 
-import java.util.Date;
-
+//ZEND JS
 public class zend {
 
 	public static void main(String[] args) {
 		try {
 			SendMessage sendMessage = new SendMessage("juano", "123456", "http-remoting://localhost:8080", "jms/queue/prodandesescribir");
-			sendMessage.sendMessage("Este es el mensaje 0 " + new Date());
-			ReceiveMessage receiveMessage = new ReceiveMessage("juano", "123456", "http-remoting://localhost:8080", "jms/queue/prodandesescribir");
-			receiveMessage.startReceiving();
-			receiveMessage.closeConnection();
+			sendMessage.sendMessage("Hola cabroncillo hijodeputa");
+//			ReceiveMessage receiveMessage = new ReceiveMessage("juano", "123456", "http-remoting://localhost:8080", "jms/queue/prodandesescribir");
+//			receiveMessage.startReceiving();
+//			receiveMessage.closeConnection();
 		} catch(Exception e)
 		{
 			e.printStackTrace();
